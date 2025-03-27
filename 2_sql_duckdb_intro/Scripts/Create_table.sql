2d6267c5e41ce4079801207113ceea98e3deec01
Roberto C. <roberto.camacho4@gmail.com>
2025-03-27 13:43:57 +0100

    Added comment in sql file

---
+++ 2_sql_duckdb_intro/Scripts/Create_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE jokes (
+    id INTEGER PRIMARY KEY,
+    joke_text VARCHAR,
+    rating INTEGER
+);
+
+SELECT * FROM jokes;
---
+++ 2_sql_duckdb_intro/Scripts/delete.sql
@@ -0,0 +1,10 @@
+SELECT * FROM jokes;
+
+SELECT * FROM jokes WHERE rating < 5;
+
+DELETE FROM jokes WHERE rating < 5;
+
+
+
+
+DELETE FROM jokes;
