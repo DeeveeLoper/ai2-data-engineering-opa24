b7fdeb536de8a45c54368dcef8e777c30ef5f955
Roberto C. <roberto.camacho4@gmail.com>
2025-03-27 13:52:51 +0100

    Added comment in sql file

--- 3_analyze_csv_duckdb/INGEST_DATA/sql_scripts/read_csv.sql
+++ 3_analyze_csv_duckdb/INGEST_DATA/sql_scripts/read_csv.sql
@@ -1,4 +1,4 @@
--- (T-SQL (Microsoft SQL Server).
+-- (T-SQL (Microsoft SQL Server). Added the code in duckdb
 CREATE TABLE youtube AS
 SELECT * FROM read_csv_auto('data/social media influencers - youtube.csv');
 
