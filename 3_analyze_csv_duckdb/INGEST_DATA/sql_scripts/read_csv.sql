7118a83c656aafa40991a57892694db27989f453
Roberto C. <roberto.camacho4@gmail.com>
2025-03-27 13:36:18 +0100

    Changed the  sql file

--- 3_analyze_csv_duckdb/INGEST_DATA/sql_scripts/read_csv.sql
+++ 3_analyze_csv_duckdb/INGEST_DATA/sql_scripts/read_csv.sql
@@ -1,4 +1,4 @@
--- Fungerar ej (T-SQL (Microsoft SQL Server)).
+-- (T-SQL (Microsoft SQL Server).
 CREATE TABLE youtube AS
 SELECT * FROM read_csv_auto('data/social media influencers - youtube.csv');
 
