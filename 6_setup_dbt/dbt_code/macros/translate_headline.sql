754b4001b5d26be143b6c04a32d88c283545e8d5
Roberto C. <roberto.camacho4@gmail.com>
2025-04-25 14:44:56 +0200

    Add translate_title macro for job title standardization

--- 6_setup_dbt/dbt_code/macros/translate_headline.sql
+++
@@ -1,6 +0,0 @@
-{% macro translate_headline(column_name) %}
-    CASE
-        WHEN {{ column_name }} = 'Data Engineer' THEN 'Junior data engineer'
-        ELSE {{ column_name }}
-    END
-{% endmacro %}
---
+++ 6_setup_dbt/dbt_code/macros/translate_title.sql
@@ -0,0 +1,6 @@
+{% macro translate_title(column_name) %}
+    CASE
+        WHEN {{ column_name }} = 'Data Engineer' THEN 'Junior Data Engineer'
+        ELSE {{ column_name }}
+    END
+{% endmacro %}
